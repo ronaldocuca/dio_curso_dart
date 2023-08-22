e6ee47b810b3a31140fd60099b7729d5a1411958
Ronaldo Rocha <ronaldocuca@gmail.com>
2023-08-21 22:47:11 -0300

    feat: add dart

---
+++ bin/dio_curso_dart.dart
@@ -0,0 +1,5 @@
+import 'package:dio_curso_dart/dio_curso_dart.dart' as dio_curso_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dio_curso_dart.calculate()}!');
+}
---
+++ test/dio_curso_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:dio_curso_dart/dio_curso_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
